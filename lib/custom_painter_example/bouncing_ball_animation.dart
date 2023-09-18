add56f10b963c22fdc8014c492dc5f9988d3f317
Rivaan Ranawat <namanrivaan@gmail.com>
2023-09-18 08:42:29 -0400

    uploading bouncing ball animation starter code

---
+++ lib/custom_painter_example/bouncing_ball_animation.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+class BouncingBallAnimation extends StatefulWidget {
+  const BouncingBallAnimation({super.key});
+
+  @override
+  State<BouncingBallAnimation> createState() => _BouncingBallAnimationState();
+}
+
+class _BouncingBallAnimationState extends State<BouncingBallAnimation> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.end,
+          children: [
+            // Need Ball
+          ],
+        ),
+      ),
+    );
+  }
+}
